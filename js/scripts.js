a4b21a0db56e437185707eb83e8408dda3a330ce
Victoria045 <victoriaberyl45@gmail.com>
2021-09-01 08:42:46 +0300

    set up js files

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+// Addition
+var add = (number1, number2) => number1 + number2
+
+
+var number1 = parseInt(prompt('Enter a number:'));
+var number2 = parseInt(prompt('Enter the second number'));
+alert(add(number1, number2));
+
+// subtraction
+var subtract = (number3, number4) => number3 - number4
+
+
+var number3 = parseInt(prompt('Enter a number:'));
+var number4 = parseInt(prompt('Enter the second number'));
+alert(subtract(number3, number4));
+
+// Multiplication
+var multiply = (number5, number6) => number5 * number6
+
+
+var number5 = parseInt(prompt('Enter a number:'));
+var number6 = parseInt(prompt('Enter the second number'));
+alert(multiply(number5, number6));
+
+// Division
+var divide = (number7, number8) => number7 / number8
+
+
+var number7 = parseInt(prompt('Enter a number:'));
+var number8 = parseInt(prompt('Enter the second number'));
+alert(divide(number7, number8));
